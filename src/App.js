283b09dfba39e0eca06d69c184bd31852e8fb1c2
Sathish000009 <kumarsathish1399@gmail.com>
2023-06-26 02:31:01 +0530

    react app deploy

--- src/App.js
+++ src/App.js
@@ -1,132 +1,17 @@
-import Card from "./card.js";
-
-function App() {
-let myPrices = [
-  {
-    plan: "FREE",
-    price: "$0/month",
-    features: [
-      {
-        name: "Single User",
-        isEnabled: true
-      },
-      {
-        name: "5GB Storage",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Public Projects",
-        isEnabled: true
-      },
-      {
-        name: "Community Access",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Private Projects",
-        isEnabled: false
-      },
-      {
-        name: "Dedicated Phone Support",
-        isEnabled: false
-      },
-      {
-        name: "Free Subdomain",
-        isEnabled: false
-      },
-      {
-        name: "Monthly Status Reports",
-        isEnabled: false
-      }
-    ]
-  },
-  {
-    plan: "PLUS",
-    price: "$9/month",
-    features: [
-      {
-        name: "5 Users",
-        isEnabled: true
-      },
-      {
-        name: "50GB Storage",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Public Projects",
-        isEnabled: true
-      },
-      {
-        name: "Community Access",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Private Projects",
-        isEnabled: true
-      },
-      {
-        name: "Dedicated Phone Support",
-        isEnabled:true        },
-      {
-        name: "Free Subdomain",
-        isEnabled:true        },
-      {
-        name: "Monthly Status Reports",
-        isEnabled: false        }
-    ]
-  },
-  {
-    plan: "PRO",
-    price: "$49/month",
-    features: [
-      {
-        name: "Unlimited Users",
-        isEnabled: true
-      },
-      {
-        name: "150GB Storage",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Public Projects",
-        isEnabled: true
-      },
-      {
-        name: "Community Access",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Private Projects",
-        isEnabled: true
-      },
-      {
-        name: "Dedicated Phone Support",
-        isEnabled: true
-      },
-      {
-        name: "Unlimited Free Subdomains",
-        isEnabled: true
-      },
-      {
-        name: "Monthly Status Reports",
-        isEnabled: true
-      }
-    ]
-  }
-]
- return (
-    <>
-        <section className="pricing py-5">
-          <div className="container">
-            <div className="row">
-              {
-                myPrices.map( (obj) =>{return<Card data={obj}></Card>})   // store data in myPrices map to card 
-              }
-            </div>
-          </div>
-        </section>
-    </>
-  );
+import React from 'react';
+import Header from "./Header";
+import Footer from "./footer";
+import Section from "./Section";
+import Navbar from "./Navbar";
+function App(props) {
+    const { notes } = props;
+return (
+        <div className="App">
+            <Navbar/>
+            <Header />
+            <Section notes={notes}/>
+            <Footer />
+        </div>
+    )
 }
-
 export default App;
